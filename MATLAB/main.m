a74ea08a3a400a4eee1b69a6ee1a1ea2a052d5dc
Razzamatazz3722 <32124725+Razzamatazz3722@users.noreply.github.com>
2020-09-17 21:20:02 +1000

    added working launch files and new burger

--- MATLAB/main.m
+++ MATLAB/main.m
@@ -8,8 +8,18 @@ while(1)
     msg2 = receive(sub,10);
     %convert from ros image type to matlab image type
     img = readImage(msg2);
+    roi = [470,300,720,620];
+    [msg,~,loc] = readBarcode(img,roi,"QR-CODE");
     
-    imshow(img);
+    xyText =  loc(2,:);
+    Imsg = insertText(img,xyText,msg,"BoxOpacity",1,"FontSize",25);
+    
+    Imsg = insertShape(Imsg, "FilledCircle", [loc, ...
+     repmat(10, length(loc), 1)],"Color","red","Opacity",1);
+    
+    imshow(Imsg)
+    
+    
     
     
     
