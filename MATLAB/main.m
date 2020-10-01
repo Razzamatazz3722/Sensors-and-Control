98252def570f23a862059193d8e21cfe2e7e01db
Razzamatazz3722 <32124725+Razzamatazz3722@users.noreply.github.com>
2020-10-01 15:32:50 +1000

    started adding the controller code to the main.m file. Updated transformation code to round point of interest

--- MATLAB/main.m
+++ MATLAB/main.m
@@ -4,15 +4,40 @@ rosinit
 
 follower_message_prefix = "/tb3_0/"
 
-size_x = 640;
-size_y = 480;
-
-%setup parameters
-%sub = rossubscriber('/tb3_0/camera/parameter_updates');
-%pause(1);
 
 
+%% Initialise variables
+%From image processing
+%X = 1;          %X coord of object in camera coord frame. [head]
+%Y = 1;          %Y coord of object in camera coord frame. [turn]
+%Z = 1;          %Z coord of object in camera coord frame. [height]
+%depth_ri = 1;   %Depth raw image; depth from camera 
+%P_x = 1;        %Selected point coord in the image frame. 
+%P_y = 1;        %Selected point coord in the image frame. 
+u = 1;          %New reading of x value of selected point coord.
+v = 1;          %New reading of y value of selected point coord. 
 
+size_x = 1392; %size of the image
+size_y = 1024;
+
+loc = []; %initialise blank array
+
+%PID variables
+kp = 1;                 %Proportional gain value.
+ki = 1;                 %Integral gain value.
+kd = 1;                 %Differential gain value. 
+
+des_depth = 1;      %Following distance. Ex. 1 meter or 1 unit. 
+des_angle = 0;      %Desired 0 angle difference.
+
+linV_max = 0.26;    %linear velocity max speed.
+linV_min = 0;
+
+angV_max = 0.05;%1.82;    %Right turn max speed. 
+angV_min = -0.05;%-1.82;   %Left turn max speed. 
+
+
+%%
 while(1)
     %setup ros subscriber
     sub = rossubscriber('/tb3_0/camera/rgb/image_raw');
@@ -24,13 +49,19 @@ while(1)
     img = readImage(msg1);
 
     I_gs = convertRGBtoGS(img);
-    I_bw = convertGStoBW(I_gs, 0.1);
+    I_bw = imadjust(I_gs, [0 0.1]);
+    %I_bw = convertGStoBW(I_gs, 0.1);
 
     imwrite(I_bw , ['../MATLAB/',  'robot', '.jpg'],'jpg');
 
     new_I = imread("robot.jpg");
     
     offset = 20;
+    
+    if length(loc) > 0
+        u = loc(1,1); %set u and v to the previous loc point
+        v = loc(1,2);
+    end;
 
     [msg,~,loc] = readBarcode(new_I,"QR-CODE");
     if length(loc) == 0
@@ -45,7 +76,6 @@ while(1)
     Imsg = insertShape(Imsg, "Line", [(size_x/2)-offset,0,(size_x/2)-offset,size_y], "Color", "green", "LineWidth", 2);
 
     imshow(Imsg);
-    %disp('process')
     
     %%Transformation to 3D coordinates
     
@@ -56,11 +86,15 @@ while(1)
     msg2 = receive(sub2,10);
     
     %convert from ros image type to matlab image type
-    depth_raw_image = readImage(msg2);
+    depth_ri = readImage(msg2);
     
     %pick first loc point. bottom left square. Point of interest
-    p_x = loc(1,1)
-    p_y = loc(1,2)
+    P_x = loc(1,1);
+    P_y = loc(1,2);
+    
+    P_x = round(P_x);
+    P_y = round(P_y);
+    
     
     
     %get rgb camera information
@@ -69,10 +103,45 @@ while(1)
     
     msg3 = receive(sub3,10);
      
-    [X,Y,Z] = transform_image_to_3D(p_x, p_y, msg3, depth_raw_image)
+    [X,Y,Z] = transform_image_to_3D(P_x, P_y, msg3, depth_ri);
      
     
-     
+    %Initialise bank data
+    errorDepthBank = [0;0;0;0];
+    errorAngleBank = [0;0;0;0];
+
+    %Calculate Error for depth and angle.    
+    err_d = calculateErrorDepth(X,Y,des_depth);
+    [err_aWorld,err_aImage,direction] = calculateErrorAngle(X,Y,u,v,P_x,P_y);
+
+    %Storing error values in a container. 
+    flagA = 5;
+    errorDepthBank(flagA,:) = err_d;
+    errorAngleBank(flagA,:) = err_aImage;
+    flagA = flagA + 1;
+
+    %Calculate PID Controller values
+    Pd = proportionalController(kp,errorDepthBank);
+    Pa = proportionalController(kp,errorAngleBank);
+
+    Id = integralController(ki,errorDepthBank);
+    Ia = integralController(ki,errorAngleBank);
+
+    Dd = differentialController(kd,errorDepthBank);
+    Da = differentialController(kd,errorAngleBank);
+    
+    disp("linear velocity")
+    out_d = calculateOutput(Pd,Id,Dd,linV_max,linV_min,1)
+    disp("angular velocity")
+    out_a = calculateOutput(Pa,Ia,Da,angV_max,angV_min,direction)
+
+    %Publish to cmd/vel
+    cmd_msg = rosmessage('geometry_msgs/Twist');
+    %cmd_msg.Linear.X = out_d;
+    cmd_msg.Angular.Z = out_a;
+
+    pubVel = rospublisher('/tb3_0/cmd_vel', 'geometry_msgs/Twist');
+    send(pubVel,cmd_msg);
      
 
 %     %TEST ROTATING TO A POINT
@@ -105,4 +174,52 @@ while(1)
 %     pub = rospublisher('/tb3_0/cmd_vel', 'geometry_msgs/Twist');
 %     send(pub,cmd_msg);
 %     
+end
+
+
+%% FUNCTIONS
+function P = proportionalController(kp,errorBank)
+    round = size(errorBank);
+    P = kp*errorBank(round(1));
+end
+
+function I = integralController(ki,errorBank)
+    sumError = sum(errorBank);
+    I = ki*sumError;
+end
+
+function D = differentialController(kd,errorBank)
+    round = size(errorBank);
+    slope = errorBank(round(1)) - errorBank(round(1)-1);
+    D = kd*slope;
+end
+
+function out = calculateOutput(P,I,D,maxOut,minOut,direction)
+    out = P+I+D * direction;
+    if out > maxOut
+        out = maxOut;
+    end
+    
+    if out < minOut
+        out = minOut;
+    end 
+end
+
+function [err_aWorld,err_aImage,direction] = calculateErrorAngle(X,Y,u,v,P_x,P_y)
+    err_aWorld = atan2d(X,Y);
+    err_aImage = atan2d((P_x-u),(P_y-v)); %Assuming x coord is not facing forward.
+    if P_x - u > 0
+        direction = 1;                    %Object is on the left.
+        disp("object on left")
+    else
+        direction = -1;                   %Object is on the right.
+        disp("object on right")
+    end
+    
+end
+
+function err_d = calculateErrorDepth(X,Y,setDepth)
+    coord = [0 0;X,Y];
+    eucDistance = pdist(coord,'euclidean');
+    err_d = setDepth - eucDistance;
 end
--- MATLAB/transform_image_to_3D.m
+++ MATLAB/transform_image_to_3D.m
@@ -1,14 +1,14 @@
 function [X,Y,Z] = transform_image_to_3D(image_x, image_y, raw_camera_info, input_depth_image)
 
     %point of interest on image
-    p_x = image_x;
-    p_y = image_y;
+    p_x = image_x
+    p_y = image_y
     
     %raw depth image
-    img = input_depth_image;
+    raw_depth = input_depth_image;
     
     %depth at point of interest
-    depth = img(p_x, p_y);
+    depth = raw_depth(p_x, p_y)
     
     %camera calibration matrix
     K_matrix = [raw_camera_info.K(1,1), raw_camera_info.K(2,1), raw_camera_info.K(3,1);
