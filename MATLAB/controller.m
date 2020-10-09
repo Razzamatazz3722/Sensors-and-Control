fc769a431e92be85015542427903ae379c7238c2
reboje <rebojsantos@gmail.com>
2020-10-09 16:30:16 +1100

    OG Test Successful

--- MATLAB/controller.m
+++ MATLAB/controller.m
@@ -1,7 +1,15 @@
 % PID CONTROLLER
 function controller()
 
+clearvars
+rosshutdown
+rosinit
+
+follower_message_prefix = "/tb3_0/"
+
+
 %% ASSUMED COORDINATES
+% From /tb3_0/base_link
 % x [HEAD AXIS]
 % ^
 % |
@@ -10,8 +18,8 @@ function controller()
 %% Initialise variables
 %From image processing
 head_val = 1;          %X coord of object in camera coord frame. [head]
-side_val = 1;          %Y coord of object in camera coord frame. [turn]
-turn_val = 1;          %Z coord of object in camera coord frame. [height]
+side_val = 0;          %Y coord of object in camera coord frame. [turn]
+turn_val = 0;          %Z coord of object in camera coord frame. [height]
 
 x = 1;                 %Orientation x 
 y = 1;
@@ -19,9 +27,13 @@ z = 1;
 w = 1;
 
 %PID variables
-kp = 1;                 %Proportional gain value.
-ki = 1;                 %Integral gain value.
-kd = 1;                 %Differential gain value. 
+kp_d = 0.5;                 %Proportional gain value.
+ki_d = 0.05;                 %Integral gain value.
+kd_d = 0.1;                 %Differential gain value. 
+
+kp_a = 0.005;                 %Proportional gain value.
+ki_a = 0.0003;                 %Integral gain value.
+kd_a = 0.002;                 %Differential gain value. 
 
 set_depth = 1;          %Following distance. Ex. 1 meter or 1 unit. 
 set_angle = 0;          %Desired 0 angle difference.
@@ -36,6 +48,7 @@ angV_min = -1.82;       %Left turn max speed.
 errorDepthBank = [0;0;0;0];
 errorAngleBank = [0;0;0;0];
 
+flagA = 5;
 
 %% FUNCTIONS
 function P = proportionalController(kp,errorBank)
@@ -67,7 +80,10 @@ end
 
 function [err_a,direction] = calculateErrorAngle(side_val,head_val)
     err_a = atan2d(side_val,head_val);
-    if side_val > 0         %Object on Right     
+    if err_a < 0
+        err_a = 0;
+    end
+    if side_val < 0         %Object on Right     
         direction = -1;    
     else                    %Object on Left
         direction =  1;   
@@ -89,7 +105,12 @@ function err_d = calculateErrorDepth(side_val,head_val,setDepth)
     coord = [0 0;side_val,head_val];
     eucDistance = pdist(coord,'euclidean');
     err_d = eucDistance - setDepth;
+    if err_d < 0
+        err_d = 0;
+    end
 end
+
+
 
 
 while(1)   
@@ -104,17 +125,16 @@ errorAngleBank(flagA,:) = err_a;
 flagA = flagA + 1;
     
 %Calculate PID Controller values
-Pd = proportionalController(kp,errorDepthBank);
-Pa = proportionalController(kp,errorAngleBank);
+Pd = proportionalController(kp_d,errorDepthBank);
+Pa = proportionalController(kp_a,errorAngleBank);
 
-Id = integralController(ki,errorDepthBank);
-Ia = integralController(ki,errorAngleBank);
+Id = integralController(ki_a,errorDepthBank);
+Ia = integralController(ki_a,errorAngleBank);
 
-Dd = differentialController(kd,errorDepthBank);
-Da = differentialController(kd,errorAngleBank);
+Dd = differentialController(kd_d,errorDepthBank);
+Da = differentialController(kd_a,errorAngleBank);
 
 out_d = calculateOutput(Pd,Id,Dd,linV_max,linV_min,1);
-
 out_a = calculateOutput(Pa,Ia,Da,angV_max,angV_min,direction);
 
 %Publish to cmd/vel
