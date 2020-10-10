705540309408059b5737fdafca2fde40961ce10f
Razzamatazz3722 <32124725+Razzamatazz3722@users.noreply.github.com>
2020-10-10 13:11:04 +1100

    integrated controller with the ar track alvar package

--- MATLAB/main2.m
+++ MATLAB/main2.m
@@ -23,21 +23,21 @@ kp_a = 0.005;                 %Proportional gain value.
 ki_a = 0.0003;                 %Integral gain value.
 kd_a = 0.002;                 %Differential gain value. 
 
-set_depth = 1;          %Following distance. Ex. 1 meter or 1 unit. 
+set_depth = 0.65;          %Following distance. Ex. 1 meter or 1 unit. 
 set_angle = 0;          %Desired 0 angle difference.
 
-linV_max = 0.05;%0.26;        %linear velocity max speed.
+linV_max = 0.26;        %linear velocity max speed.
 linV_min = 0;
 
-angV_max = 0.1;%1.82;        %Right turn max speed. 
-angV_min = -0.1;%-1.82;       %Left turn max speed. 
+angV_max = 1.82;        %Right turn max speed. 
+angV_min = -1.82;       %Left turn max speed. 
 
                         %Initialise bank data
 errorDepthBank = [0;0;0;0];
 errorAngleBank = [0;0;0;0];
 
-errorDepthBank2 = {0,0,0,0};
-errorAngleBank2 = {0,0,0,0};
+errorDepthBank2 = {0,0,0,0,0};
+errorAngleBank2 = {0,0,0,0,0};
 
 flagA = 5;
 
@@ -62,10 +62,13 @@ while(1)
     end
     
     %assign PID controller object values
-    head_val = msg1.Markers(1).Pose.Pose.Position.Z; %X coord of object in camera coord frame.
+    head_val = msg1.Markers(1).Pose.Pose.Position.X; %X coord of object in camera coord frame.
     side_val = msg1.Markers(1).Pose.Pose.Position.Y; %Y coord of object in camera coord frame.
-    turn_val = msg1.Markers(1).Pose.Pose.Position.X; %Z coord of object in camera coord frame.
-
+    turn_val = msg1.Markers(1).Pose.Pose.Position.Z; %Z coord of object in camera coord frame.
+    
+    disp("distance = " + head_val);
+    disp("side = " + side_val);
+    
 %     obj_z = head_val;
 %     obj_y = side_val;
 %     obj_x = turn_val;x
@@ -112,27 +115,31 @@ while(1)
 %     send(pub,cmd_msg);
     
     
-    
-    
-    
-    
-    
     %Calculate Error for depth and angle.    
     err_d = calculateErrorDepth(side_val,head_val,set_depth);
     [err_a,direction] = calculateErrorAngle(side_val,head_val);
+    
+    disp("direction = " + direction);
 
-    %Storing error values in a container. 
+    %Storing error values in a container.
     flagA = 5;
     errorDepthBank(flagA,:) = err_d;
     errorAngleBank(flagA,:) = err_a;
     flagA = flagA + 1;
+    
+    %disp(errorDepthBank2);
+    %errorDepthBank2 = errorDepthBank2(2:end);
+    %errorDepthBank2{end+1} = err_d;
+    %disp(errorDepthBank2);
+    
+    
    
-   
+
     %Calculate PID Controller values
     Pd = proportionalController(kp_d,errorDepthBank);
     Pa = proportionalController(kp_a,errorAngleBank);
 
-    Id = integralController(ki_a,errorDepthBank);
+    Id = integralController(ki_d,errorDepthBank);
     Ia = integralController(ki_a,errorAngleBank);
 
     Dd = differentialController(kd_d,errorDepthBank);
@@ -157,6 +164,8 @@ end
 function P = proportionalController(kp,errorBank)
     increment_err = size(errorBank);
     P = kp*errorBank(increment_err(1));
+    %tmp = errorBank{5};
+    %P = kp*tmp;
 end
 
 function I = integralController(ki,errorBank)
@@ -171,7 +180,9 @@ function D = differentialController(kd,errorBank)
 end
 
 function out = calculateOutput(P,I,D,maxOut,minOut,direction)
-    out = P+I+D * direction;            %-1-> Turn right; 1-> Turn Left
+    out = P+I+D * direction;           %-1-> Turn right; 1-> Turn Left
+    disp("P=" + P + "  I=" + I + "  D=" + D ); 
+    %out = P*direction;
     if out > maxOut
         out = maxOut;
     end
@@ -183,9 +194,9 @@ end
 
 function [err_a,direction] = calculateErrorAngle(side_val,head_val)
     err_a = atan2d(side_val,head_val);
-    if err_a < 0
-        err_a = 0;
-    end
+    %if err_a < 0
+    %    err_a = 0;
+    %end
     if side_val < 0         %Object on Right     
         direction = -1;    
     else                    %Object on Left
