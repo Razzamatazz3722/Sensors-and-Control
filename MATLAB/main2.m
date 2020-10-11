17d8838d62f94a0a2ba8c6b3b60ce896f6988308
Razzamatazz3722 <32124725+Razzamatazz3722@users.noreply.github.com>
2020-10-11 16:09:52 +1100

    added more comments

--- MATLAB/main2.m
+++ MATLAB/main2.m
@@ -23,10 +23,12 @@ z = 1;
 w = 1;
 
 %PID variables
+% Distance Gain Values
 kp_d = 0.5;                     % Proportional gain value.
 ki_d = 0.05;                    % Integral gain value.
 kd_d = 0.1;                     % Differential gain value. 
 
+% Angular Gain Values
 kp_a = 0.005;                   % Proportional gain value.
 ki_a = 0.0003;                  % Integral gain value.
 kd_a = 0.002;                   % Differential gain value. 
@@ -85,20 +87,20 @@ while(1)
     errorAngleBank(flagA,:) = err_a;
     flagA = flagA + 1;
 
-    %Calculate PID Controller values
-    Pd = proportionalController(kp_d,errorDepthBank);
-    Pa = proportionalController(kp_a,errorAngleBank);
+    %Calculate PID Controller values for depth and angle
+    Pd = proportionalController(kp_d,errorDepthBank); %depth proportional controller value
+    Pa = proportionalController(kp_a,errorAngleBank); %angular proportional controller value
 
-    Id = integralController(ki_d,errorDepthBank);
-    Ia = integralController(ki_a,errorAngleBank);
+    Id = integralController(ki_d,errorDepthBank); %depth integral controller value
+    Ia = integralController(ki_a,errorAngleBank); %angular integral controller value
 
-    Dd = differentialController(kd_d,errorDepthBank);
-    Da = differentialController(kd_a,errorAngleBank);
+    Dd = differentialController(kd_d,errorDepthBank); %depth differential controller value
+    Da = differentialController(kd_a,errorAngleBank); %angular differential controller value
+    
+    out_d = calculateOutput(Pd,Id,Dd,linV_max,linV_min,1); %linear velocity output value
+    out_a = calculateOutput(Pa,Ia,Da,angV_max,angV_min,direction); %angular velocity output value
 
-    out_d = calculateOutput(Pd,Id,Dd,linV_max,linV_min,1);
-    out_a = calculateOutput(Pa,Ia,Da,angV_max,angV_min,direction);
-
-    %Publish to cmd/vel
+    %Create rosmessage and publish to cmd/vel
     cmd_msg = rosmessage('geometry_msgs/Twist');
     cmd_msg.Linear.X = out_d;
     cmd_msg.Angular.Z = out_a;
@@ -107,30 +109,54 @@ while(1)
     pubVel = rospublisher('/tb3_0/cmd_vel', 'geometry_msgs/Twist');
     send(pubVel,cmd_msg);    
 
-end
+end 
 
 
 %% FUNCTIONS
+
+% Name: proportionalController
+% Inputs: kp - proportional gain value, errorBank - matrix of accumulated 
+% errors
+% Outputs: P - proportional controller value
+% Description: Calculates the proportional value of the controller. NOTE:
+% it is assumed that latest error is held in the last/bottom position in the
+% matrix. 
 function P = proportionalController(kp,errorBank)
     increment_err = size(errorBank);
     P = kp*errorBank(increment_err(1));
 end
 
+% Name: integralController
+% Inputs: ki - integral gain value, errorBank - matrix of accumulated
+% errors
+% Outputs: I - integral controller value
+% Description: Calculates the integral value of the controller
 function I = integralController(ki,errorBank)
     sumError = sum(errorBank);
     I = ki*sumError;
 end
 
+% Name: differentialController
+% Inputs: kd - differential gain value, errorBank - matrix of acucmulated
+% errors
+% Outputs: D - derivatice controller value
+% Description: Calculates the derivatice value of the controller
 function D = differentialController(kd,errorBank)
     increment_err = size(errorBank);
     slope = errorBank(increment_err(1)) - errorBank(increment_err(1)-1);
     D = kd*slope;
 end
 
+% Name: calculateOutput
+% Inputs: P - proportional controller value, I - integral controller value,
+% D - differential controller value, maxOut - maximum output value, minOut
+% - minimum output value, direction - sets the polarity of the output
+% Outputs: out - output value to control the robot
+% Description: calculates the output value based on the PID values and the
+% minimum and maximum output values. 
 function out = calculateOutput(P,I,D,maxOut,minOut,direction)
     out = P+I+D * direction;           %-1-> Turn right; 1-> Turn Left
     disp("P=" + P + "  I=" + I + "  D=" + D ); 
-    %out = P*direction;
     if out > maxOut
         out = maxOut;
     end
@@ -140,6 +166,14 @@ function out = calculateOutput(P,I,D,maxOut,minOut,direction)
     end 
 end
 
+% Name: calculateErrorAngle
+% Inputs: side_val - the value of the side coordinate of the object of
+% interest, head_val - the value of the head coordinate of the object of 
+% interest. 
+% Outputs: err_a - angle error, direction - positive if the point is on the 
+% left of the centre of the follower robot, negative if the point is on the 
+% right of the centre of the follower robot
+% Description: Calculates the current angle error to the point of interest
 function [err_a,direction] = calculateErrorAngle(side_val,head_val)
     err_a = atan2d(side_val,head_val);
     %if err_a < 0
@@ -152,6 +186,13 @@ function [err_a,direction] = calculateErrorAngle(side_val,head_val)
     end    
 end
 
+% Name: calculateErrorDepth
+% Inputs: side_val - the value of the side coordinate of the object of
+% interest, head_val - the value of the head coordinate of the object of 
+% interest. 
+% Outputs: err_d - depth error
+% Description: calculates the depth error between the robots current
+% position and the point of interest
 function err_d = calculateErrorDepth(side_val,head_val,setDepth)
     coord = [0 0;side_val,head_val];
     eucDistance = pdist(coord,'euclidean');
